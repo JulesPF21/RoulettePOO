a7b9a95612c2ee651895a97f8fad4c19ee8bd0b6
Jules Petit Faucher <jules.petitfaucher@gmail.com>
2025-02-21 10:50:06 +0100

    playerManager+HeroManager

--- Assets/Scripts/HeroManager.cs
+++ Assets/Scripts/HeroManager.cs
@@ -1,6 +1,48 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class HeroManager
 {
-    
+    [SerializeField] private int damageAmount;
+    [SerializeField] private int wallDamageAmount;
+    [SerializeField] private int healing;
+    [SerializeField] private int wallHealing;
+    [SerializeField] private int attackHeight;
+    [SerializeField] private bool isHealer;
+    [SerializeField] private bool isKnight;
+    [SerializeField] private bool isMage;
+    [SerializeField] private LevelManager levelManager;
+    [SerializeField] private PlayerManager playerManager;
+
+    virtual public void Attack()
+    {
+        if (isHealer)
+        { 
+            playerManager.heal(healing);
+        }
+
+        if (isKnight)
+        {
+            if (playerManager.wallHeight <= 0)
+            {
+                playerManager.damage(damageAmount);
+            }
+
+            if (playerManager.wallHeight > 0)
+            { 
+                playerManager.wallDamage(wallDamageAmount);
+            }
+        }
+
+        if (isMage)
+        {
+            if (playerManager.wallHeight < attackHeight)
+            { 
+                playerManager.damage(damageAmount);
+            }
+            if (playerManager.wallHeight > attackHeight)
+            {
+                playerManager.wallDamage(wallDamageAmount);
+            }
+        }
+    }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class PlayerManager
 {
-    
+    public int wallHeight;
+    [SerializeField] private int playerHealth;
+    [SerializeField] private HeroManager heroManager;
+
+
+    public void heal(int healAmount)
+    {
+        playerHealth += healAmount;
+    }
+
+    public void damage(int damageAmount)
+    {
+        playerHealth -= damageAmount;
+    }
+
+    public void wallHeal(int wallHealAmount)
+    {
+        wallHeight += wallHealAmount;
+    }
+
+    public void wallDamage(int wallDamageAmount)
+    {
+        wallHeight -= wallDamageAmount;
+    }
 }
